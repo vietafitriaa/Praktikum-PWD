7b4b8421e0924a3e33aed4542fba509c236d0503
vietafitriaa <50800018+vietafitriaa@users.noreply.github.com>
2021-10-07 23:51:18 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-ede">
+    <meta name="viewport" content="width-device-width, initial-scale-1.0">
+</head>
+
+<body>
+<form enctype="multipart/form-data" method="post" action="upload.php">
+File yang diupload: <input type="file" name="fupload"><br>
+Deskripsi File: <br><textarea name="deskripsi" rows="6" cols="20"></textarea><br>
+<input type=submit value=Upload>
+</form>
+
+<br>
+<a href="download.php">klik untuk download</a>
+
+</body>
+</html>
